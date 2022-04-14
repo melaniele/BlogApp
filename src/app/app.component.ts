f5cf18a170d86e006a243aaecf2bc211d2cadbae
Melanie Le <77984641+melaniele@users.noreply.github.com>
2022-04-14 11:17:44 -0400

    comment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 /*********************************************************************************
-* BTI425 – Assignment 03
+* BTI425 – Assignment 04
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
 * assignment has been copied manually or electronically from any other source (including web sites) or 
 * distributed to other students.
 * 
-* Name: Thi Hong Minh Le (Melanie) | Student ID: 103087201 | Date: March 24th, 2022
+* Name: Thi Hong Minh Le (Melanie) | Student ID: 103087201 | Date: April 14th, 2022
 *
 ********************************************************************************/ 
 import { Component } from '@angular/core';
@@ -15,5 +15,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bti425-a3-melaniele';
+  title = 'bti425-a4-melaniele';
 }
